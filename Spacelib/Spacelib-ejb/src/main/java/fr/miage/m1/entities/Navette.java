4feb818fba12b052a223ca1d28aa992d204d7080
valentin-chevalier <74769967+valentin-chevalier@users.noreply.github.com>
2021-05-26 11:45:34 +0200

    Added entities Navette, Operation

--- Spacelib/Spacelib-ejb/src/main/java/fr/miage/m1/entities/Navette.java
+++ Spacelib/Spacelib-ejb/src/main/java/fr/miage/m1/entities/Navette.java
@@ -6,10 +6,12 @@
 package fr.miage.m1.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 
 /**
  *
@@ -22,6 +24,26 @@ public class Navette implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+    private boolean estEnRevision;
+    private int nbVoyages;
+    private int capacite;
+    private int idQUai;
+    @OneToMany(mappedBy = "navette")
+    private ArrayList<Operation> listeOperations;
+    
+    public Navette() {
+    }
+
+    public Navette(Long id, boolean estEnRevision, int nbVoyages, int capacite, ArrayList<Operation> listeOperations, int idQUai) {
+        this.id = id;
+        this.estEnRevision = estEnRevision;
+        this.nbVoyages = nbVoyages;
+        this.capacite = capacite;
+        this.listeOperations = listeOperations;
+        this.idQUai = idQUai;
+    }
+    
+    
 
     public Long getId() {
         return id;
@@ -54,6 +76,46 @@ public class Navette implements Serializable {
     @Override
     public String toString() {
         return "fr.miage.m1.entities.Navette[ id=" + id + " ]";
+    }
+
+    public boolean isEstEnRevision() {
+        return estEnRevision;
+    }
+
+    public void setEstEnRevision(boolean estEnRevision) {
+        this.estEnRevision = estEnRevision;
+    }
+
+    public int getNbVoyages() {
+        return nbVoyages;
+    }
+
+    public void setNbVoyages(int nbVoyages) {
+        this.nbVoyages = nbVoyages;
+    }
+
+    public int getCapacite() {
+        return capacite;
+    }
+
+    public void setCapacite(int capacite) {
+        this.capacite = capacite;
+    }
+
+    public ArrayList<Operation> getListeOperations() {
+        return listeOperations;
+    }
+
+    public void setListeOperations(ArrayList<Operation> listeOperations) {
+        this.listeOperations = listeOperations;
+    }
+
+    public int getIdQUai() {
+        return idQUai;
+    }
+
+    public void setIdQUai(int idQUai) {
+        this.idQUai = idQUai;
     }
     
 }
